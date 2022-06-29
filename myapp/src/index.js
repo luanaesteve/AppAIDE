8503bf895b872f7eb872d061e74fd63b6305fbcf
luana <luanaesteve@hotmail.com>
2022-06-28 21:06:24 -0300

    continuando

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LoginForm from "./UI/Components/form";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App(){
+    return(
+        <View style={styles.container}>
+            <Text></Text>
+            <Text></Text>
+            <LoginForm/>
+        </View>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
