cfdc00d934431f5cf7df6e1ab418b2cc408e9a9b
luana <luanaesteve@hotmail.com>
2022-06-28 21:25:06 -0300

    haciendo js

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,26 +6,29 @@ import './App.css';
 
 function App() {
   const adminUSer = {
-    
+    email: "admin@admin.com",
+    password: "admin1234"
+  }
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+
+  const login = details => {
+    console.log(details);
+  }
+  const Logout = () =>{
+    console.log("Logout");
   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <form>
+      <div className="form-inner">
+        <h2>Login</h2>
+        {/*ERROR! */}
+        <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input type="text" name="name"></>
+        </div>
 
+      </div>
+    </form>
+  )
 export default App;
