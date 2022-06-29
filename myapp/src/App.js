04b4a336d3d6ff4ed61cdf34350ad580dba1f43d
luana <luanaesteve@hotmail.com>
2022-06-28 21:33:30 -0300

    haciendo commits

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,21 @@
-
 import React from 'react';
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './UI/Components/form';
 
 function App() {
   const adminUSer = {
     email: "admin@admin.com",
     password: "admin1234"
   }
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState({name: "", email: ""});
   const [error, setError] = useState("");
 
   const login = details => {
     console.log(details);
   }
-  const Logout = () =>{
+  const Logout = () => {
     console.log("Logout");
   }
   return (
@@ -24,11 +24,19 @@ function App() {
         <h2>Login</h2>
         {/*ERROR! */}
         <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name"></>
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name"/>
         </div>
-
+        <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" id="email"/>
+        </div>
+        <div className="form-group">
+              <label htmlFor="password">Password:</label>
+              <input type="password" name="password" id="password"/>
+        </div>
       </div>
     </form>
   )
-export default App;
+}
+export default LoginForm;
